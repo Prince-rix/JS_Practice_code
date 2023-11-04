cf351111a5cb84458fb4a5e880941dd0b6144c02
Prince-rix <princerix486@gmail.com>
2023-11-04 23:24:20 +0530

    Remove duplicate

--- Array.js
+++ Array.js
@@ -1,7 +1,20 @@
-let b = [1,2,22,3,123]
-b.forEach(element => {
-    if(element > 1){
-    console.log(element)
-    }
-});
+let b = [1,2,1,2,22,3,123]
+let temp;
+let Arr= [];
+b.sort();
 
+// b.map((e)=>{
+//   if(temp !=e){
+//     Arr.push(e)
+//     temp=b
+//   }
+// })
+
+for( let i=0;i<b.length;i++){
+  if(temp!=b[i]){
+    Arr.push(b[i])
+    temp=b[i]
+  }
+}
+
+console.log(Arr)
